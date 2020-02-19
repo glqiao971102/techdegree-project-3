75a08835773eda1467de8f39bf7b145f76db2cb8
glqiao971102 <44012470+glqiao971102@users.noreply.github.com>
2020-02-19 19:38:34 +0800

    T-Shirt Info” section

--- js/script.js
+++ js/script.js
@@ -14,3 +14,32 @@ $('#title').on("change", () => {
         $('#other-title').slideDown();
     }
 });
+
+//”T-Shirt Info” section
+
+$('#color').hide();
+$('#color').html('<option>Please select a T-shirt theme</option>');
+$('#color').show();
+
+$('#design').on("change", () => {
+    
+    if($('#design option:selected').text() == "Theme - JS Puns")
+    {
+        
+        $('#color').hide();
+        $('#color').html('<option value="cornflowerblue">Cornflower Blue (JS Puns shirt only)</option><option value="darkslategrey">Dark Slate Grey (JS Puns shirt only)</option> <option value="gold">Gold (JS Puns shirt only)</option> ');
+        $('#color').show();
+        
+        
+    }else if($('#design option:selected').text() == "Theme - I ♥ JS"){
+        $('#color').hide();
+        $('#color').html('<option value="tomato">Tomato (I &#9829; JS shirt only)</option><option value="steelblue">Steel Blue (I &#9829; JS shirt only)</option><option value="dimgrey">Dim Grey (I &#9829; JS shirt only)</option> ');
+        $('#color').show();
+    }else {
+
+        $('#color').hide();
+        $('#color').html('<option>Please select a T-shirt theme</option>');
+        $('#color').show();
+
+    }
+});
