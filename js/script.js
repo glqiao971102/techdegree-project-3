499e645fa5dee3cdd79288da602fd135599af6ed
glqiao971102 <44012470+glqiao971102@users.noreply.github.com>
2020-02-21 15:25:44 +0800

    register for activity section

--- js/script.js
+++ js/script.js
@@ -43,3 +43,96 @@ $('#design').on("change", () => {
 
     }
 });
+
+
+//”Register for Activities” section
+const checkboxes = $('.activities label input')
+
+$(".addClass").prop("checked", false);
+$(".addClass").click(function () {
+
+        if ($(this).is(":checked")) {
+
+            //checked
+            $(this).addClass("selected");
+
+        } else {
+            //unchecked
+            $(this).removeClass("selected");
+        }
+        
+        let totalPrice = 0
+        for(let i = 0; i < checkboxes.length; i ++){
+            
+            
+            if(checkboxes[i].className == 'addClass selected'){
+    
+                totalPrice += parseInt(checkboxes[i].getAttribute('data-cost'))
+                
+            }
+    
+            
+        }
+
+        $('.totalPrice').html(`<p>Total Price: ${totalPrice}</p>`)
+
+    })
+
+checkboxes.on('change', e => {
+
+    const clicked = e.target;
+    let clickedName = clicked.getAttribute('name');
+    
+    if ($('input[name="js-frameworks"]').is(':checked')){
+
+        $('input[name="express"]').attr('disabled', true)
+
+    }else {
+
+        $('input[name="express"]').attr('disabled', false)
+
+    }
+
+    if ($('input[name="express"]').is(':checked')){
+
+        $('input[name="js-frameworks"]').attr('disabled', true)
+
+    }else {
+
+        $('input[name="js-frameworks"]').attr('disabled', false)
+
+    }
+
+    if ($('input[name="js-libs"]').is(':checked')){
+
+        $('input[name="node"]').attr('disabled', true)
+
+    }else {
+
+        $('input[name="node"]').attr('disabled', false)
+
+    }
+
+    if ($('input[name="node"]').is(':checked')){
+
+        $('input[name="js-libs"]').attr('disabled', true)
+
+    }else {
+
+        $('input[name="js-libs"]').attr('disabled', false)
+
+    }
+    
+
+    
+    //$(clicked).attr('disabled', true)
+
+
+    
+
+    console.log(clicked)
+    console.log(clickedName)
+    
+    
+
+})
